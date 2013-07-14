22241b71373a6e8963f44d46de53d3213a77769d
Jesse Zhang <jthereliable@gmail.com>
2013-07-14 15:55:31 -0400

    rest api

--- server/node/rest.js
+++ server/node/rest.js
@@ -1,12 +1,81 @@
-var express		= require("express");
+var express		= require("express"),
+	async		= require("async"),
+	fs			= require("fs"),
+	settings	= require("settings");
+var debug_mode = (settings.server.environment == "debug");
 
 var app = express();
-app.get("/", function(req, res) {
-	console.log("/");
-	res.send("Hello!");
+
+var paths = [];
+function scanDirectory(dir, cb)
+{
+	fs.readdir(dir, function(err, files) {
+		if(err)
+		{
+			throw(err);
+		}
+		async.each(files, function(item, next) {
+			if(item == "route.js")
+			{
+				paths.push(dir);
+				next();
+				return;
+			}
+			fs.lstat(dir+"/"+item, function(err, stats) {
+				if(err)
+				{
+					throw(err);
+				}
+				if(stats.isDirectory() && !stats.isSymbolicLink())
+				{
+					scanDirectory(dir+"/"+item, next);
+					return;
+				}
+				next();
+			});
+		}, function(err) {
+			if(err)
+			{
+				throw(err);
+			}
+			cb();
+		});
+	});
+}
+
+var methods = ["get","post","put","del"];
+scanDirectory("./rest", function() {
+	var i, l=methods.length, method, path, rest;
+	async.each(paths, function(dir, next) {
+		path = dir.replace(/^\.\/rest/,"").replace(/\/\!/g,"/:")+"/?";
+		rest = require(dir+"/route.js");
+		for(i=0;i<l;i++)
+		{
+			method = methods[i];
+			if(rest[method] instanceof Function)
+			{
+				if(debug_mode)
+				{
+					(function() {
+						var _path = path;
+						var _rest = rest;
+						var _method = method;
+						app[_method](_path, function(req, res) {
+							console.log("Requesting: " + _path);
+							_rest[_method](req, res);
+						});
+					})();
+				} else {
+					app[method](path, rest[method]);
+				}
+			}
+		}
+		next();
+	}, function(err) {
+		if(err)
+		{
+			throw(err);
+		}
+		app.listen(82);
+	});
 });
-app.get("/test/?", function(req, res) {
-	console.log("/test/?");
-	res.send("Test!");
-});
-app.listen(82);
---
+++ server/node/rest/route.js
@@ -0,0 +1,6 @@
+exports.get = function(req, res) {
+	res.json({
+		"api": "Feel the Beats Beta 3 REST API",
+		"version": "0.0.1"
+	});
+};
