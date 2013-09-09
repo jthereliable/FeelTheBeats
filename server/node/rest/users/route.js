3a30559269b94344f8f8742d5e510fadf5c8a0d1
Jesse Zhang <jthereliable@gmail.com>
2013-09-09 12:54:42 -0400

    simplifying rest api

--- server/node/rest/users/route.js
+++ server/node/rest/users/route.js
@@ -4,17 +4,21 @@ var mysql				= require("database/mysql.js"),
 	logger				= require("debug/logger.js");
 
 var get_params_order = ["uid", "name", "tier", "points", "experience"];
-var get_params_query = ["name", "tier", "mod_level", "charter_level"];
+var get_params_query = {
+	"name": {
+		"string": true
+	},
+	"tier": {},
+	"mod_level": {},
+	"charter_level": {}
+};
 var get_params_query_string = ["name"];
 exports.get = function(req, res) {
-	var limits = requestable.limits(req, get_params_order, 25);
-	var where = requestable.where(req, get_params_query, get_params_query_string);
-	
 	mysql.query("SELECT `uid`, `name`, `image`, `tier`, `mod_level`, `charter_level`, `date_join`, `date_login`			\
-				FROM `Users` WHERE "+where+"																			\
-				ORDER BY ?? "+limits.sort+"																				\
-				LIMIT ?, ?",
-	[limits.order, limits.offset, limits.limit], function(err, rows) {
+				FROM `Users`" +
+				requestable.where(req, get_params_query) +
+				requestable.limits(req, get_params_order, 25),
+	function(err, rows) {
 		if(err)
 		{
 			res.json({
